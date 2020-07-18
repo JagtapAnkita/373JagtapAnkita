e6cac02e5b5f0d5d9ea54054f2bd25a0922227ed
JagtapAnkita <68368112+JagtapAnkita@users.noreply.github.com>
2020-07-18 16:15:45 +0530

    Add files via upload

---
+++ Ass1_C.py
@@ -0,0 +1,12 @@
+N1=int(input("Enter the first number: "))
+N2=int(input("Enter the second number: "))
+print("1st Number=",N1)
+print("2nd Number=",N2)
+
+Add=N1+N2
+Diff=N1-N2
+Mul=N1*N2
+
+print("Sum: ",Add)
+print("Diff: ",Diff)
+print("Product: ",Mul)
