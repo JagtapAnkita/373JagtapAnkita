2df618f47bd9f45b6deba54d059aa8bb7ad9e04d
JagtapAnkita <68368112+JagtapAnkita@users.noreply.github.com>
2020-07-18 15:42:22 +0530

    Add files via upload

---
+++ Ass1_B.py
@@ -0,0 +1,21 @@
+my_list=[11,2,9,4,50]
+print("created list is: ",my_list)
+
+my_list.insert(5,10)
+print("After insertion: ",my_list)
+
+my_list.remove(10)
+print("After deletion: ",my_list)
+
+my_list.append(20)
+print("After Append:",my_list)
+
+my_list.sort()
+print("After sorting",my_list
+
+my_list.pop()
+print("After Poping",my_list)
+
+my_list.reverse()
+print("After reversing: ",my_list)
+
